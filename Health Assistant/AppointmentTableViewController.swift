017d1f545150e4423bb19d57b49c2c2d4472ed1c
Victoria Saklakova <viksi.saklakova@gmail.com>
2020-04-18 17:18:30 +0300

    Add Doctors to 2nd tab

---
+++ Health Assistant/AppointmentTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AppointmentTableViewController.swift
+//  Health Assistant
+//
+//  Created by Виктория Саклакова on 18.04.2020.
+//  Copyright © 2020 Viktoriia Saklakova. All rights reserved.
+//
+
+import UIKit
+
+class AppointmentTableViewController: UITableViewController {
+
+    var appointmentArray = ["Saturday 13 Apr", "Sunday 15 May"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return appointmentArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AppointmentCell", for: indexPath)
+
+        cell.textLabel?.text = appointmentArray[indexPath.row]
+
+        return cell
+    }
+
+}
