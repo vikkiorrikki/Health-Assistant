088fef1eb0ea573b50420547dbd4366ed2b3208e
Victoria Saklakova <viksi.saklakova@gmail.com>
2020-04-18 15:54:58 +0300

    Initial Commit

---
+++ Health Assistant/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Health Assistant
+//
+//  Created by Виктория Саклакова on 18.04.2020.
+//  Copyright © 2020 Viktoriia Saklakova. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
